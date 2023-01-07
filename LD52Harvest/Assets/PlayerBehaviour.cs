d87168880dab8ea1906656deb6f596c701b87900
JakobRW <jakobrw@outlook.com>
2023-01-07 05:08:20 +0100

    Push

--- LD52Harvest/Assets/PlayerBehaviour.cs
+++ LD52Harvest/Assets/PlayerBehaviour.cs
@@ -16,13 +16,16 @@ public class PlayerBehaviour : MonoBehaviour
     float rotationSmoothing;
 
     public float rotationTimer;
+
+    public bool isDashing;
+    bool canDash = true;
     
     void Start() {
         
     }
 
     void Update() {
-        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+        if (Input.GetKeyDown(KeyCode.UpArrow) && !isDashing) {
             speed = 6f;
         }
         if (Input.GetKeyUp(KeyCode.UpArrow)) {
@@ -32,16 +35,27 @@ public class PlayerBehaviour : MonoBehaviour
             rotationTimer = 0;
         }
 
+        if (Input.GetKeyDown(KeyCode.X) && canDash) {
+            canDash = false;
+            isDashing = true;
+            StartCoroutine(Dash());
+            speed = 25f;
+        }
+
         if (Input.GetKey(KeyCode.RightArrow) && !Input.GetKey(KeyCode.LeftArrow)) {
             animator.SetInteger("TurnDir", 1);
             rotationTimer += Time.deltaTime;
             
             myRotation -= 0.8f;
-            if (rotationTimer > 0.3f) {
-                myRotation -= 0.3f;
-            }
-            if (rotationTimer > 0.6f) {
-                myRotation -= 0.3f;
+            if (!isDashing) {
+                if (rotationTimer > 0.3f) {
+                    myRotation -= 0.3f;
+                }
+                if (rotationTimer > 0.6f) {
+                    myRotation -= 0.3f;
+                }
+            } else {
+                myRotation -= 2f;
             }
         }
         if (Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.RightArrow)) {
@@ -49,11 +63,15 @@ public class PlayerBehaviour : MonoBehaviour
             rotationTimer += Time.deltaTime;
             
             myRotation += 0.8f;
-            if (rotationTimer > 0.3f) {
-                myRotation += 0.3f;
-            }
-            if (rotationTimer > 0.6f) {
-                myRotation += 0.3f;
+            if (!isDashing) {
+                if (rotationTimer > 0.3f) {
+                    myRotation += 0.3f;
+                }
+                if (rotationTimer > 0.6f) {
+                    myRotation += 0.3f;
+                }
+            } else {
+                myRotation += 2f;
             }
         }
 
@@ -74,11 +92,23 @@ public class PlayerBehaviour : MonoBehaviour
             rotationTimer = 0;
         }
 
-        speedToSet = Mathf.SmoothDamp(speedToSet, speed, ref speedSmoothing, 0.2f);
+        speedToSet = Mathf.SmoothDamp(speedToSet, speed, ref speedSmoothing, isDashing ? 0f : 0.2f);
         rb2d.velocity = transform.up * speedToSet;
 
         rotationToSet = Mathf.SmoothDamp(rotationToSet, myRotation, ref rotationSmoothing, 0.1f);
         transform.eulerAngles = new Vector3(0, 0, rotationToSet);
+    }
+
+    IEnumerator Dash() {
+        yield return new WaitForSeconds(0.2f);
+        isDashing = false;
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            speed = 6f;
+        } else {
+            speed = 0f;
+        }
+        yield return new WaitForSeconds(0.5f);
+        canDash = true;
     }
 
     void FixedUpdate() {
